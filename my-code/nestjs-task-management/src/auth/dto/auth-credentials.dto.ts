a9ccd7621025a441a5d6e96704a2272bf883bc5e
Mkozi <mkozi.junk@gmail.com>
2020-05-22 08:16:01 -0500

    Section 5: end

---
+++ my-code/nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString, MinLength, MaxLength, Matches } from "class-validator";
+
+export class AuthCredentialsDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'password to weak'})
+  password: string;
+}
